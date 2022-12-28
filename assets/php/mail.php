6b2257707bde733a6c5c5e960e2fcbffc4f7a5f5
Daniel Garcia Perez <36474611+DaniGarPe@users.noreply.github.com>
2022-12-28 14:14:16 +0100

    update works

--- assets/php/mail.php
+++ assets/php/mail.php
@@ -13,10 +13,29 @@
 
         $message = wordwrap($message, 70, "\r\n");
 
+        $clean_message = spam_scrubber($message);
+
+        $additional_headers = 'From: ' . $mail . "\r\n" . "Name: " . $name;
+
+        $clean_message = wordwrap($clean_message, 70, "\r\n");
+
         mail( "contact@danielweb.es", $subject, $message, $additional_headers );
 
         echo "<script type='text/javascript'>alert('El email se envio correctamente.'); window.location.href = 'https://danielweb.es#contact';</script>";
         
+    }
+
+    function spam_scrubber($value) {
+        // Palabras y caracteres comunes en el spam
+        $spam_words = array('viagra', 'cialis', 'poker', 'casino', 'xxx', 'porn', '$$$', 'http', 'https', 'dinero', 'financi', 'message');
+        $spam_characters = array('#', '%', '&', '*');
+      
+        // Elimina las palabras y caracteres comunes del mensaje
+        $value = str_ireplace($spam_words, '', $value);
+        $value = str_ireplace($spam_characters, '', $value);
+      
+        // Devuelve el mensaje sin spam
+        return $value;
     }
 
 ?>
